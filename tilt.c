a53b438478d498cc22e2f40a2ef0d731e48c173f
cook0286 <cook0286@flinders.edu.au>
2014-05-08 15:46:42 +0930

    Addes logic for shifting

--- tilt.c
+++ tilt.c
@@ -7,6 +7,9 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
+  
+  // While the first value in line is 0 shift value left
+  // if (line[i] == line[i+1] || line[i] == line[i+2] || line[i] == line[i+3])
 
   // combine tiles as required
 
